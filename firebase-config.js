7aa61edadefb2b6abfad0795cfd194c3aee2592a
Thiago <thiagod11lopes@gmail.com>
2026-02-22 19:16:25 -0300

    Corrigir 404 no GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/**
+ * Configuração do Firebase - SISMAV
+ * Cole aqui o objeto firebaseConfig que você copiou do Firebase Console
+ * (Configurações do projeto > Seus aplicativos > Config)
+ */
+const firebaseConfig = {
+  apiKey: "COLE_SUA_API_KEY_AQUI",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
